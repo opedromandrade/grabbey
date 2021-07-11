36945ed59c6b203f411f40bf1ea735bcb0f85907
opedromandrade <pmm.andrade@outlook.com>
2021-07-11 16:32:25 +0100

    folderz

---
+++ scripts/nux/DeskTopz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# 
+# Author: Pedro Andrade - https://github.com/opedromandrade
+# this is just a simple script for Ubuntu
+#
+# v02.2021
+# updated on: 07.2021
+#
+# before starting:
+#	1. make sure you have the fastest mirror selected, and also the Canonical Partners Repo enabled
+#	2. comment lines out to disable stuff you don't want install/remove
+
+
+echo $'\n'$"*** Follow the white rabbit & reboot ***"
+exit
